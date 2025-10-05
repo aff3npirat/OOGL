bbcb903842c7b52bd1d330033270e72fe5be72f3
aff3npirat <jurek.elliesen@web.de>
2025-10-05 10:10:42 +0200

    Implement OGL Buffers

---
+++ source/render_context.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <utility>
+
+#include <GL/Glew.h>
+
+
+struct Buffer {
+    struct Base {
+        virtual ~Base() = 0;
+
+        unsigned int size;
+        void* values;
+    };
+
+    template<typename T>
+    struct Implement : Base {
+        Implement(unsigned int size) {
+            values = new T[size];
+            this->size = size;
+        }
+
+        T* values;
+        unsigned int size;
+        GLuint uid;
+    };
+
+    ~Buffer() { delete ptr; }
+    template<typename T> void init(std::in_place_type_t<T>, unsigned int size) { ptr = new Implement<T>(size); }
+    unsigned int byteSize() { return ptr->size; }
+    void* data() { return (void*)ptr->values; }
+
+    Base* ptr;
+};
+
+
+class BufferView {
+  public:
+    BufferView(unsigned int stride, unsigned int offset, unsigned int vertexSize, int glType) {
+        this->stride = stride;
+        this->offset = offset;
+        this->vertexSize = vertexSize;
+        this->glType = glType;
+    }
+
+    template<typename T>
+    void insert(T* values, unsigned int size, Buffer buf) {
+        T* first = static_cast<T*>(buf.data())[offset];
+
+        for (int i = 0; i < size; i += vertexSize) {
+            for (int j = 0; j < vertexSize; j++) {
+                first[i*stride + j] = values[i + j];
+            }
+        }
+    }   
+
+  private:
+    unsigned int stride;
+    unsigned int offset;
+    unsigned int vertexSize;
+    int glType;
+};
